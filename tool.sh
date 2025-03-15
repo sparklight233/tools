f174dee9397b5dc85b4ab11181490a76060fe4b0
Spark <200543674+sparklight233@users.noreply.github.com>
2025-03-15 14:10:42 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -162,7 +162,6 @@ if docker inspect "$docker_name" &>/dev/null; then
     echo "0. 返回上一级选单"
     echo "------------------------"
     read -p $'\033[1;91m请输入你的选择: \033[0m' sub_choice
-}
 
     case $sub_choice in
         1)
@@ -229,7 +228,6 @@ else
             ;;
     esac
 fi
-
 
 while true; do
 clear
