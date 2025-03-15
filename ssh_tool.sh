3beb80894e5605159cb76ffe0de08758b29b9e4b
Spark <200543674+sparklight233@users.noreply.github.com>
2025-03-15 20:13:09 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -548,8 +548,9 @@ case $choice in
     cd ~
     rm ssh_tool.sh
     curl -fsSL https://raw.githubusercontent.com/sparklight233/tools/refs/heads/main/ssh_tool.sh -o ssh_tool.sh && chmod +x ssh_tool.sh
-    echo -e "${green}脚本已更新到最新版本！${re}"
     sleep 1
+    echo -e "${green}脚本已更新到最新版本！${re}"
+    sleep 2
     ./ssh_tool.sh
     ;;
     
