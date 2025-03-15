250565053febd9a9b0bcc52bdf41c55a988e590c
Spark <200543674+sparklight233@users.noreply.github.com>
2025-03-15 15:04:53 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -404,8 +404,10 @@ case $choice in
              ;;
           8)
               clear
-              curl -O https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh || wget -O reinstall.sh $_
-              bash reinstall.sh debian12 --password Lyx12345@
+              apt update -y
+              apt install wget -y
+              wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh' && chmod a+x InstallNET.sh
+              bash InstallNET.sh -debian 12 -pwd Lyx12345@ -hostname debian12 -port 51888
             ;;
           9)
               clear
