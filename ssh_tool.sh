b220d9f263e630d4f4d6871dc170dc12a75fad56
Spark <200543674+sparklight233@users.noreply.github.com>
2025-03-17 10:17:18 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -473,6 +473,7 @@ case $choice in
           13)
               clear
               wget https://raw.githubusercontent.com/Jimmyzxk/DNS-Alice-Unlock/refs/heads/main/dns-unlock.sh && bash dns-unlock.sh
+              rm dns-unlock.sh
             ;;
           14)
               clear
