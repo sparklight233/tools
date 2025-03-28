7a1035533dd31983bf0eb23492806dc7f9ee1a8b
Spark <200543674+sparklight233@users.noreply.github.com>
2025-03-28 16:06:51 +0800

    Update magic.sh

--- sh/magic.sh
+++ sh/magic.sh
@@ -49,6 +49,13 @@ apt install ufw -y
 ufw default allow outgoing 
 ufw default deny incoming
 
+# 设置虚拟内存
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab 
+
 # 安装Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
